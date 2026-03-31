cf9576d91ff93201b77882ad775e394a18fff9c8
s33440-cpu <s33440@pjwstk.edu.pl>
2026-03-31 16:56:13 +0200

    Add project files.

---
+++ LegacyRenewalApp/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LegacyRenewalApp;
+using LegacyRenewalApp.Models;
+
+namespace LegacyRenewalApp.Interfaces
+{
+    public interface ICustomerRepository 
+    {
+        Customer GetById(int customerId);
+    }
+}
---
+++ LegacyRenewalApp/Interfaces/IRenewalServiceValidator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegacyRenewalApp.Interfaces
+{
+    public interface IRenewalServiceValidator
+    {
+        void Validate(
+        int customerId,
+        string planCode,
+        int seatCount,
+        string paymentMethod);
+    }
+}
